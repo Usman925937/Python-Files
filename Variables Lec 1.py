0d2b6dc66cb971b8afa0554600e0e7c44a32ca0f
Muhammad Usman <168414331+Usman925937@users.noreply.github.com>
2024-07-08 21:21:09 +0500

    Add files via upload

---
+++ Variables Lec 1.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+print ('Hello World')
+
+
+# In[22]:
+
+
+x = 'Chocolate Chip'
+
+
+# In[23]:
+
+
+print (x)
+
+
+# # Right Way
+
+# In[17]:
+
+
+X4 = 20
+print (X4)
+
+
+# # Wrong Way
+
+# In[ ]:
+
+
+4X = 40
+
+
+# In[ ]:
+
+
+
+
+
+# # Variables
+
+# In[21]:
+
+
+type (X4)
+
+
+# In[24]:
+
+
+type (x)
+
+
+# In[ ]:
+
+
+
+
+
+# # Assigning Multiple Variables 
+
+# In[27]:
+
+
+x,y,z = 'Vanilla', 'Pista', 'Chocolate Fudge'
+print (x)
+print (y)
+print (z)
+
+
+# In[28]:
+
+
+x = y = z = 'Root Beer Float'
+print (x)
+print (y)
+print (z)
+
+
+# In[31]:
+
+
+ice_cream = ['Kulfa', 'Butter Scotch', 'Blueberry']
+x,y,z = ice_cream
+print (x)
+print (y)
+print (z)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
